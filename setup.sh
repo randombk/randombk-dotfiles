cbe337dedcb07d2b083f8b620e2f076d387efcd4
RandomBK <david@david-li.com>
2017-06-27 00:24:07 -0700

    impl(setup): Add nobackup option
    Signed-off-by: RandomBK <david@david-li.com>

--- setup.sh
+++ setup.sh
@@ -5,10 +5,17 @@ set -e
 SRC_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 BACKUP_DIR="$HOME/.dotfiles-backup/$(date +"%Y-%m-%d-%s")"
 
-echo Original files will be saved in $BACKUP_DIR
-mkdir -p "$BACKUP_DIR"
+if [ $1 != "nobackup" ]; then
+    echo Original files will be saved in $BACKUP_DIR
+    mkdir -p "$BACKUP_DIR"
+fi
 
 ls -A1 "$SRC_DIR/dotfiles" | while read i; do
-	! mv "$HOME/$i" "$BACKUP_DIR" 2>/dev/null
-	ln -s "$SRC_DIR/dotfiles/$i" "$HOME/$i"
+    if [ $1 != "nobackup" ]; then
+        ! mv "$HOME/$i" "$BACKUP_DIR" 2>/dev/null
+    else
+        ! rm -rf -- "$HOME/$i"
+    fi
+
+    ln -s "$SRC_DIR/dotfiles/$i" "$HOME/$i"
 done
