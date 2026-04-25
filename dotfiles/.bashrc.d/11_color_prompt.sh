f9caae54c3689a17cb124574ac97aa081b3886e6
David Li <jiawei.davidli@gmail.com>
2026-04-25 15:38:43 -0700

    Revert "Add dedicated agent mode"

--- dotfiles/.bashrc.d/11_color_prompt.sh
+++ dotfiles/.bashrc.d/11_color_prompt.sh
@@ -1,26 +1,22 @@
 #
 # Force Color Prompt Implementation
 #
+
 if [ -n "$force_color_prompt" ]; then
-    if [ "$IS_AGENT" = true ]; then
-        # Use dark blue for agent
-        export PS1='\\[\033[1;34m\\](\\[\033[0;37m\\]\u\\[\033[1;34m\\]@\h\\[\033[1;34m\\]:\\[\033[1;36m\\]\w\\[\033[1;34m\\])\\[\033[1;36m\\]\$ \\[\033[0;37m\\]'
-        export color_prompt=yes
-    elif [ "$_isroot" = true ]; then
-        # Use green for root
-        export PS1='\\[\033[1;32m\\](\\[\033[1;32m\\]\u\\[\033[1;32m\\]@\h\\[\033[1;32m\\]:\\[\033[1;36m\\]\w\\[\033[1;32m\\])\\[\033[1;36m\\]\$ \\[\033[0;37m\\]'
-        export PS2='#->'
-        export color_prompt=yes
+    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+	# We have color support; assume it's compliant with Ecma-48
+	# (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+	# a case would tend to support setf rather than setaf.)
+	export color_prompt=yes
     else
-        # Default: red for user
-        export PS1='\\[\033[1;31m\\](\\[\033[0;37m\\]\u\\[\033[1;31m\\]@\h\\[\033[1;31m\\]:\\[\033[1;36m\\]\w\\[\033[1;31m\\])\\[\033[1;36m\\]\$ \\[\033[0;37m\\]'
-        export color_prompt=yes
+	export color_prompt=
     fi
 fi
 
 #
 # More Colors
 #
+
 export TERM=xterm-color
 unset GREP_OPTIONS # Grep now breaks with this on
 export CLICOLOR=1
@@ -30,12 +26,12 @@ export LSCOLORS=ExFxCxDxBxegedabagacad
 export force_color_prompt=yes
 
 # Custom Prompt
-export PS1='\\[\033[1;31m\\](\\[\033[0;37m\\]\u\\[\033[1;31m\\]@\h\\[\033[1;31m\\]:\\[\033[1;36m\\]\w\\[\033[1;31m\\])\\[\033[1;36m\\]\$ \\[\033[0;37m\\]'
+export PS1='\[\033[1;31m\](\[\033[0;37m\]\u\[\033[1;31m\]@\h\[\033[1;31m\]:\[\033[1;36m\]\w\[\033[1;31m\])\[\033[1;36m\]\$ \[\033[0;37m\]'
 export PS2='-->'
 
 # Override for root
 if $_isroot; then
-    export PS1='\\[\033[1;32m\\](\\[\033[1;32m\\]\u\\[\033[1;32m\\]@\h\\[\033[1;32m\\]:\\[\033[1;36m\\]\w\\[\033[1;32m\\])\\[\033[1;36m\\]\$ \\[\033[0;37m\\]'
+    export PS1='\[\033[1;32m\](\[\033[1;32m\]\u\[\033[1;31m\]@\h\[\033[1;31m\]:\[\033[1;36m\]\w\[\033[1;32m\])\[\033[1;36m\]\$ \[\033[0;37m\]'
     export PS2='#->'
 fi
 
